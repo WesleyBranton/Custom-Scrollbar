d5bfeabbaaba5c4150c4305e0c090288225c2bd2
Wesley Branton <addons@wesleybranton.com>
2022-02-06 21:34:07 -0500

    Fixed content scripts not loading after add-on is enabled (#131)

--- src/background.js
+++ src/background.js
@@ -401,6 +401,9 @@ if (typeof browser != 'object') {
     importScripts("crossbrowser.js", "defaults.js", "generateCSS.js");
 }
 
+if (typeof browser.storage.session == 'object') {
+    init(false);
+}
 browser.runtime.onInstalled.addListener(handleInstalled);
 browser.runtime.onMessage.addListener(handleMessage);
 browser.runtime.onStartup.addListener(() => {
